105e55f104231ca91c7b7f817837e55345c57148
Karol Moroz <karol@moroz.dev>
2024-04-04 22:40:21 +0800

    Same but with action group

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use gio::ActionEntry;
+use glib::clone;
+use gtk::gio::SimpleActionGroup;
 use gtk::prelude::*;
 use gtk::{gio, glib, Application, ApplicationWindow};
 
@@ -7,7 +9,7 @@ const APP_ID: &str = "org.gtk_rs.Actions1";
 fn main() -> glib::ExitCode {
     let app = Application::builder().application_id(APP_ID).build();
     app.connect_activate(build_ui);
-    app.set_accels_for_action("win.close", &["<Ctrl>W"]);
+    app.set_accels_for_action("custom-group.close", &["<Ctrl>W"]);
     app.run()
 }
 
@@ -19,12 +21,14 @@ fn build_ui(app: &Application) {
         .build();
 
     let action_close = ActionEntry::builder("close")
-        .activate(|window: &ApplicationWindow, _, _| {
-            println!("Triggered by keyboard accelerator.");
+        .activate(clone!(@weak window => move |_, _, _| {
             window.close();
-        })
+        }))
         .build();
-    window.add_action_entries([action_close]);
+
+    let actions = SimpleActionGroup::new();
+    actions.add_action_entries([action_close]);
+    window.insert_action_group("custom-group", Some(&actions));
 
     window.present();
 }
